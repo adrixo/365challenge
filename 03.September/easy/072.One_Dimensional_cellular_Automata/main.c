2d4f7b6fa6593aabcc803d545060366e6cc4a9ae
adrixo <adrianvalrom@gmail.com>
2018-09-11 10:51:34 +0200

    72nd challenge: 110 rule

---
+++ 03.September/easy/072.One_Dimensional_cellular_Automata/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 256
+
+int main(void){
+
+	int iterations, i;
+
+	int board[SIZE];
+	for(i=0; i<SIZE; i++)
+		board[i] = 0;
+
+	printf("How many iterations do you want to represent?: ");
+	scanf("%d", &iterations);
+
+	for(i=0; i<iterations; i++)
+	{
+		
+	}
+
+}
