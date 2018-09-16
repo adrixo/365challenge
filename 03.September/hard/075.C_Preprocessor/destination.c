842a94630c5105cc9c3269796b247c23f83dfe40
adrixo <adrianvalrom@gmail.com>
2018-09-16 18:58:21 +0200

    77th challenge: C preprocessor

---
+++ 03.September/hard/075.C_Preprocessor/destination.c
@@ -0,0 +1,3 @@
+
+The following phrase is often used in web design:
+Le renard brun saute par-dessus le chien paresseux. Le nom du chien est Brian.
---
+++ 03.September/hard/075.C_Preprocessor/source.c
@@ -0,0 +1,9 @@
+
+#define DOGNAME Brian
+The following phrase is often used in web design:
+#define FRENCH
+#ifdef FRENCH //if french
+Le renard brun saute par-dessus le chien paresseux. Le nom du chien est DOGNAME.
+#else
+The brown fox jumped over the lazy dog.  The dog's name is DOGNAME.
+#endif
