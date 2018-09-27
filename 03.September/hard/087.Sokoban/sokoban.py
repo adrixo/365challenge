56ca7bab6a43ffa79a8a98d0bce220f120605a68
adrixo <adrianvalrom@gmail.com>
2018-09-27 22:55:34 +0200

    Final correction

--- 03.September/hard/087.Sokoban/sokoban.py
+++ 03.September/hard/087.Sokoban/sokoban.py
@@ -9,94 +9,83 @@
 # end: H
 
 class Sokoban(object):
-    map = ["##########",
-     "#        #",
-     "#  @  #  #",
-     "#     #  #",
-     "#  #  #  #",
-     "#  #     #",
-     "#      H #",
-     "#        #",
-     "##########"]
-     player = [1,1]
-     end = [6,7]
+    map = ["##########", "#o       #", "#  @  #  #", "#     #  #", "#  #  #  #", "#  #     #", "#      H #", "#        #", "##########"]
+    player = [1,1]
+    end = [6,7]
 
     def __init__(self):
-        for l in map:
-            print(l)
-
-        aux = map[2][3]
-        map[2] = map[2][:2] + "  " + map[2][4:]
-        map[4] = map[4][:4] + aux + map[4][5:]
-
-        for l in map:
-            print(l)
+        self.play()
 
     def printMap(self):
-        for l in map:
+        for l in self.map:
             print(l)
 
     def move(self, posX1, posY1, posX2, posY2):
-        aux = map[posX1][posY1]
-        map[posX1][posY1] = " "
-        map[posX1][posY1] = aux
+        aux = self.map[posX1][posY1]
+        self.map[posX1] = self.map[posX1][:posY1] + " " + self.map[posX1][posY1+1:]
+        self.map[posX2] = self.map[posX2][:posY2] + aux + self.map[posX2][posY2 +1:]
+
+    def placePlayer(self):
+        self.map[self.player[0]] = self.map[self.player[0]][:self.player[1]-1] + "o" + self.map[self.player[0]][self.player[1]+1:]
+
 
     def validMove(self, posX1, posY1, posX2, posY2):
         # void position
-        if map[posX2][posY2] == " ":
+        if self.map[posX2][posY2] == " ":
             return True
 
         # there's a box on the position
-        else if map[posX2][posY2] == "@":
+        elif self.map[posX2][posY2] == "@":
             Xaux = posX2-posX1
             Yaux = posY2-posY1
             #if after that there is block:
-            if map[posX2+Xaux][posY2+Yaux] == "#":
+            if self.map[posX2+Xaux][posY2+Yaux] == "#":
                 return False
-            else
+            else:
                 return True
         return False
 
     def moveABox(self, posX2, posY2):
-        if map[posX2][posY2] == "@":
+        if self.map[posX2][posY2] == "@":
                 return True
         return False
 
     def finish(self):
-        if map[end[0]][end[1]] == "@":
+        if self.map[self.end[0]][self.end[1]] == "@":
             return True
         return False
 
     def analizeKey(self, key):
-        if key == "a":
-            return [-1,0]
-        if key == "d":
-            return [1,0]
         if key == "w":
-            return [0,-1]
+            return [-1,0]
         if key == "s":
+            return [1,0]
+        if key == "a":
+            return [0,-1]
+        if key == "d":
             return [0,1]
 
         return False
 
     def play(self):
-        while not finish():
-            printMap()
+        while not self.finish():
+            self.printMap()
 
-            key = string(input("Enter a move: "))
-            moving = analizeKey(key)
+            key = input("Enter a move: ")
+            moving = self.analizeKey(key)
+            if moving != False:
+                if self.validMove(self.player[0], self.player[1], self.player[0]+moving[0], self.player[1]+moving[1]):
+                    if self.moveABox(self.player[0]+moving[0], self.player[1]+moving[1]):
+                        #move the box
+                        self.move(self.player[0]+moving[0], self.player[1]+moving[1], self.player[0]+moving[0]+moving[0], self.player[1]+moving[1]+moving[1])
+                        #move the player
+                        self.move(self.player[0], self.player[1], self.player[0]+moving[0], self.player[1]+moving[1])
+                    else:
+                        #move the player
+                        self.move(self.player[0], self.player[1], self.player[0]+moving[0], self.player[1]+moving[1])
+                    self.player[0] += moving[0]
+                    self.player[1] += moving[1]
 
-            if validMove(player[0], player[1], player[0]+moving[0], player[1]+moving[1]):
-                if moveABox(player[0]+moving[0], player[1]+moving[1]):
-                    #move the box
-                    move(player[0]+moving[0], player[1]+moving[1], player[0]+moving[0]+moving[0], player[1]+moving[1]+moving[1])
-                    #move the player
-                    move(player[0], player[1], player[0]+moving[0], player[1]+moving[1])
-                else:
-                    #move the player
-                    move(player[0], player[1], player[0]+moving[0], player[1]+moving[1])
-                player[0] += moving[0]
-                player[0] += moving[0]
-
+            #self.placePlayer()
+        print("You win!")
 game = Sokoban()
-#game.printMap()
