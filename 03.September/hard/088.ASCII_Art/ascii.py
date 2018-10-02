16ae9d478d8646f2b1ee0f6fa0c094c202281abd
adrixo <adrianvalrom@gmail.com>
2018-10-02 14:09:50 +0200

    improfs

--- 03.September/hard/088.ASCII_Art/ascii.py
+++ 03.September/hard/088.ASCII_Art/ascii.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 from PIL import Image
 
-intensitySwap = " .:-|io@"
+intensitySwap = "@oi|-:. "
 
-jpgfile = Image.open("pokeball.jpg")
+jpgfile = Image.open("s.jpg")
 
 print(jpgfile.getdata(), jpgfile.size, jpgfile.format)
 
 asciiString = ""
 n = 0
 for i in jpgfile.getdata():
-    if n % 50 == 0:
+    if n % 100 == 0:
         n=0
         asciiString+="\n"
     intensity = (i[0]*75 + i[1]*155 + i[2]*25) // 255 //32
