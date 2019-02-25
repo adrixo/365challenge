57c5688b0f225c8ee2e5376a35a450e1c5302154
adrixo <adrianvalrom@gmail.com>
2019-02-25 20:57:56 +0100

    duplicados borrados

--- 03.September/hard/075.C_Preprocessor/source.c
+++
@@ -1,9 +0,0 @@
-
-#define DOGNAME Brian
-The following phrase is often used in web design:
-#define FRENCH
-#ifdef FRENCH //if french
-Le renard brun saute par-dessus le chien paresseux. Le nom du chien est DOGNAME.
-#else
-The brown fox jumped over the lazy dog.  The dog's name is DOGNAME.
-#endif
--- 03.September/intermediate/083.Indexed_File_Search/binarySearch.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BINARYSEARCH_H
-#define __BINARYSEARCH_H
-
-int binarySearch(int *array, int length, int value);
-int recursivebinarySearch(int *array, int bot, int top, int value);
-
-#endif
